7617e0cf4e40aef18802719de7913db371df809b
Arturo <lemosart0987@gmail.com>
2025-10-01 22:47:24 -0500

    Browser renders header, show name, episode list, and the string Episode Details and prompt. Working on ShowDetails, have not been able to get it to work.

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,30 @@
  * React TV is an web streaming platform that allows users to browse
  * through the episodes of a variety of different shows.
  */
+import { useState } from "react";
+import { tvShows } from "./shows/data";
+
+import EpisodeList from "./episodes/EpisodeList";
+import EpisodeDetails from "./episodes/EpisodeDetails";
+
 export default function App() {
+  const [episodeList] = useState(tvShows);
+  const [selectedEpisode, setSelectedEpisode] = useState();
+
   return (
     <>
       <header>
         <p>React TV</p>
       </header>
-      <main></main>
+      <main>
+        <EpisodeList
+          name={tvShows[0].name}
+          episodes={tvShows[0].episodes}
+          selectedEpisode={selectedEpisode}
+          setSelectedEpisode={setSelectedEpisode}
+        />
+        <EpisodeDetails selectedEpisode={selectedEpisode} />
+      </main>
     </>
   );
 }
--- src/shows/ShowDetails.jsx
+++ src/shows/ShowDetails.jsx
@@ -1,6 +1,23 @@
 import "./shows.css";
 
 /** Allows users to browse through the episodes of the given show */
-export default function ShowDetails() {
-  return <div className="show-details"></div>;
+export default function ShowDetails({ tvShow }) {
+  const [ShowDetails] = useState();
+  if (!selectedShow) {
+    return (
+      <section>
+        <h2>Show Details</h2>
+        <p>Please select a show</p>
+      </section>
+    );
+  }
+
+  return (
+    <div className="show-details">
+      <h2>Episode List</h2>
+      <p></p>
+      <h2>Show Details</h2>
+      <p>{EpisodeDetails.description}</p>
+    </div>
+  );
 }
