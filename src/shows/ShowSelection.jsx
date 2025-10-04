d31bf8f692336bbb79ae81afb25c4b0ae8f25068
Arturo <lemosart0987@gmail.com>
2025-10-04 13:31:26 -0500

    Browser displays shows and can be clicked on. However, episode details does not render on the page. Episode details section and initial text render. No work on navbar yet.

--- src/shows/ShowSelection.jsx
+++ src/shows/ShowSelection.jsx
@@ -1,6 +1,17 @@
+import { tvShows } from "./data";
 import "./shows.css";
 
 /** A navbar that allows users to choose between a list of shows */
 export default function ShowSelection() {
-  return <nav className="shows"></nav>;
+  return (
+    <nav className="shows">
+      <ul>
+        {tvShows.map((show) => (
+          <li key={show.id} onClick={() => setSelectedShow(show)}>
+            {show.name}
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
 }
